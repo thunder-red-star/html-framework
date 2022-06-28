e64ff7c6f2716bc8c26acf90eb0ff092373ac6bc
ThunderRedStar <wvongphanith30@stuy.edu>
2022-06-28 17:39:00 -0400

    favicon, github, nav button, learn html, css and MAJOR performance improvements

--- js/button-goto.js
+++ js/button-goto.js
@@ -5,3 +5,11 @@ document.getElementById('read-the-docs').addEventListener('click', function() {
 document.getElementById('get-started').addEventListener('click', function() {
 	window.open('https://youtu.be/dQw4w9WgXcQ', '_blank')
 })
+
+document.getElementById('learn-html').addEventListener('click', function() {
+	window.open('https://www.w3schools.com', '_blank')
+})
+
+document.getElementById('nav-lh').addEventListener('click', function() {
+	window.open('https://www.w3schools.com', '_blank')
+})
